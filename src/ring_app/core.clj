a3f0d4d87694781d954b74b36d9baed8c8a359b2
yang-wei <yangweilim21@gmail.com>
2016-02-25 21:32:10 +0900

    Fresh commit

---
+++ src/ring_app/core.clj
@@ -0,0 +1,15 @@
+(ns ring-app.core
+  (:require [ring.adapter.jetty :as jetty]))
+
+(defn handler [request-map]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body (str "<html><body> your IP is: "
+              (:remore-addr request-map)
+              "</body></html>")})
+
+(defn -main []
+  (jetty/run-jetty
+   handler
+   {:port 3000
+    :join? false}))
