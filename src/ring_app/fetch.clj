a58dab3d52ddc53207e6c2c8feaad07c535169df
yang-wei <yangweilim21@gmail.com>
2016-04-13 15:54:15 +0900

    Dont forget protocol

--- src/ring_app/fetch.clj
+++ src/ring_app/fetch.clj
@@ -4,36 +4,41 @@
             [clojurewerkz.urly.core :as urly]
             [clojure.java.io :as io]))
 
-(defn resolve-url [hostname href]
+(defn resolve-url [protocol hostname href]
   (if (urly/absolute? href)
     href
-    (str hostname href)))
+    (str protocol "://" hostname href)))
 
 (defn extract-attrs [{:keys [attrs]}]
   (let [{:keys [href class]} attrs]
     {:href href :class class}))
 
-(defn build-attrs [hostname {:keys [href], :as attrs}]
-    (let [new-attr {:absolute-href (resolve-url hostname href)
+(defn build-attrs [protocol hostname {:keys [href], :as attrs}]
+    (let [new-attr {:absolute-href (resolve-url protocol hostname href)
                     :absolute-link? (urly/absolute? href)}]
      (merge attrs new-attr)))
 
 (defn fetch-url [url]
   (html/html-resource (java.net.URL. url)))
 
-(fetch-url "http://github.com/)
-
 (defn get-status-code [url]
-  (:status (client/get url)))
+  (try
+    (:status (client/get url {:throw-exceptions false :follow-redirects false}))
+    (catch Exception e
+      nil)))
 
 (defn add-status-code [{:keys [absolute-href], :as attrs}]
   (let [status (get-status-code absolute-href)]
     (assoc attrs :status status)))
 
+(defn log [{:keys [absolute-href status absolute-link?]}]
+  (spit "result.txt" (str absolute-href " " status " " absolute-link? "\n") :append true))
+
 (defn links [url]
-  (let [hostname (urly/host-of (urly/url-like url))]
+  (let [-url (urly/url-like url)
+        hostname (urly/host-of -url)
+        protocol (urly/protocol-of -url)]
     (->> (html/select (fetch-url url) [:a])
          (map extract-attrs)
-         (map (partial build-attrs hostname))
+         (map (partial build-attrs protocol hostname))
          (map add-status-code))))
-
--- test/ring_app/fetch_test.clj
+++ test/ring_app/fetch_test.clj
@@ -5,8 +5,12 @@
 (deftest resolve-url-test
   (testing "Resolve url correctly"
     (is (=
-         (fetch/resolve-url "http://example.com" "/faq")
-         "http://example.com/faq"))))
+         (fetch/resolve-url "http" "example.com" "/faq")
+         "http://example.com/faq")
+        (=
+         (fetch/resolve-url "http" "example.com" "http://example.com")
+         "http://example.com"))))
+
 
 (deftest extract-attrs-test
   (testing "Extract the needed attributes"
@@ -19,15 +23,17 @@
   (testing "Extracting useful attributes from a-node and add useful information"
 
     (testing "When href is absolute path"
-      (let [hostname "http://example.com"
+      (let [protocol "http"
+            hostname "example.com"
             node-data {:href "http://example2.com/path" :class "my-class"}]
       (is (=
-           (fetch/build-attrs hostname node-data)
+           (fetch/build-attrs protocol hostname node-data)
            {:href "http://example2.com/path" :class "my-class" :absolute-href "http://example2.com/path" :absolute-link? true}))))
 
     (testing "When href is relative path"
-      (let [hostname "http://example.com"
+      (let [protocol "https"
+            hostname "example.com"
             node-data {:href "/path" :class "my-class"}]
         (is (=
-             (fetch/build-attrs hostname node-data)
-             {:href "/path" :class "my-class" :absolute-href "http://example.com/path" :absolute-link? false}))))))
+             (fetch/build-attrs protocol hostname node-data)
+             {:href "/path" :class "my-class" :absolute-href "https://example.com/path" :absolute-link? false}))))))
