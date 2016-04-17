269098eca648b44bd3634228c9698c3338089e44
yang-wei <yangweilim21@gmail.com>
2016-04-17 13:59:49 +0900

    Separate route handler in another file

--- src/gannet/core.clj
+++ src/gannet/core.clj
@@ -1,25 +1,12 @@
 (ns gannet.core 
   (:require [ring.adapter.jetty :as jetty]
             [compojure.core :as compojure]
-            [gannet.go :as go]
-            [gannet.validate :as validate]
-            [gannet.pages :as pages]
-            [ring.util.http-response :as response]
+            [gannet.handler :refer [index-handler analyse-handler]]
             [ring.middleware.json :refer [wrap-json-response wrap-json-body]]
             [ring.middleware.reload :refer [wrap-reload]]))
 
-(defn response-handler [request-map]
-  (response/ok
-    (apply str (pages/home))))
-
-(defn analyse-handler [req]
-  (let [url (get-in req [:body :url])]
-    (if-let [valid-url? (validate/valid-url? url)]
-      (response/ok {:url url})
-      (response/forbidden "Please make sure your URL is in correct format."))))
-
 (compojure/defroutes handler
-  (compojure/GET "/" request response-handler)
+  (compojure/GET "/" request index-handler)
   (compojure/POST "/analyse" request
     (analyse-handler request)))
 
---
+++ src/gannet/handler.clj
@@ -0,0 +1,17 @@
+(ns gannet.handler
+  (:require [gannet.go :as go]
+            [gannet.validate :as validate]
+            [gannet.pages :as pages]
+            [ring.util.http-response :as response]))
+
+(defn index-handler [request-map]
+  (response/ok
+    (apply str (pages/home))))
+
+(defn analyse-handler [req]
+  (let [url (get-in req [:body :url])]
+    (if-let [valid-url? (validate/valid-url? url)]
+      (response/ok {:url url})
+      (response/forbidden "Please make sure your URL is in correct format."))))
+
+
