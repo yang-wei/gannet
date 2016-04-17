5633feba268d5b41c2ac86a021b7e5644d5471d5
yang-wei <yangweilim21@gmail.com>
2016-04-17 14:35:17 +0900

    Move method to util

--- src/gannet/go.clj
+++ src/gannet/go.clj
@@ -1,40 +1,5 @@
 (ns gannet.go
-  (:require [net.cgrand.enlive-html :as html]
-            [org.httpkit.client :as http]
-            [clojurewerkz.urly.core :as urly]
-            [clojure.java.io :as io]))
-
-(defn resolve-url [protocol hostname href]
-  (if (urly/absolute? href)
-    href
-    (str protocol "://" hostname href)))
-
-(defn extract-attrs [{:keys [attrs]}]
-  (let [{:keys [href class]} attrs]
-    {:href href :class class}))
-
-(defn build-attrs [protocol hostname {:keys [href], :as attrs}]
-  (resolve-url protocol hostname href))
-
-(defn scrap-webpage [url]
-  (html/html-resource (java.net.URL. url)))
-
-(defn extract-absolute-hrefs [url]
-  (let [-url (urly/url-like url)
-        hostname (urly/host-of -url)
-        protocol (urly/protocol-of -url)]
-    (->> (html/select (scrap-webpage url) [:a])
-         (map extract-attrs)
-         (map (partial build-attrs protocol hostname)))))
-
-(defn response-map [resp]
-  {:status (:status resp)
-   :url (-> resp :opts :url)
-   :redirects (-> resp :opts :trace-redirects)})
-
-(defn http-get [url]
-  ;; can turn off redirect flag here
-  (http/get url {:follow-redirects true}))
+  (:require [gannet.utils :refer [extract-absolute-hrefs http-get response-map]]))
 
 (defn analyse-url [url]
   (let [hrefs (extract-absolute-hrefs url)
--- test/gannet/utils_test.clj
+++ test/gannet/utils_test.clj
@@ -1,14 +1,15 @@
-(ns gannet.go-test 
+(ns gannet.utils-test 
+  (:use org.httpkit.fake)
   (:require [clojure.test :refer :all]
-            [gannet.go :as go]))
+            [gannet.utils :refer :all]))
 
 (deftest resolve-url-test
   (testing "Resolve url correctly"
     (is (=
-         (go/resolve-url "http" "example.com" "/faq")
+         (resolve-url "http" "example.com" "/faq")
          "http://example.com/faq")
         (=
-         (go/resolve-url "http" "example.com" "http://example.com")
+         (resolve-url "http" "example.com" "http://example.com")
          "http://example.com"))))
 
 
@@ -16,7 +17,7 @@
   (testing "Extract the needed attributes"
     (let [node-data {:tag "a" :attrs {:href "http://example.com" :class "my-class" :id "my-id"}}]
     (is (=
-         (go/extract-attrs node-data)
+         (extract-attrs node-data)
          {:href "http://example.com" :class "my-class"})))))
 
 (deftest build-attrs-test 
@@ -27,7 +28,7 @@
             hostname "example.com"
             node-data {:href "http://example2.com/path" :class "my-class"}]
       (is (=
-           (go/build-attrs protocol hostname node-data)
+           (build-attrs protocol hostname node-data)
            "http://example2.com/path"))))
 
     (testing "When href is relative path"
@@ -35,5 +36,5 @@
             hostname "example.com"
             node-data {:href "/path" :class "my-class"}]
         (is (=
-             (go/build-attrs protocol hostname node-data)
+             (build-attrs protocol hostname node-data)
              "https://example.com/path"))))))
