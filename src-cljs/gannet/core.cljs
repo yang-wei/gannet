e6bf384a655d17125c387e09013a0d6ee5c7a3ce
yang-wei <yangweilim21@gmail.com>
2016-04-29 14:39:53 +0900

    Use inline style css

--- src-cljs/gannet/core.cljs
+++ src-cljs/gannet/core.cljs
@@ -1,14 +1,16 @@
 (ns gannet.core
-  (:require [reagent.core :as r]))
+  (:require [reagent.core :as r
+             gannet.style :as style]))
 
 (defn form-component []
   [:div {:class "form-input"}
-   [:input {:placeholder "Paste your URL here"
+   [:input {:style style/form-input
+            :placeholder "Paste your URL here"
             :type "input"}]
-   [:button "Grab"]])
+   [:button {:style style/form-button} "Grab"]])
 
 (defn mount-app! []
   (r/render-component [form-component]
-                            (.getElementById js/document "app")))
+                      (.getElementById js/document "app")))
 
 (mount-app!)
---
+++ src-cljs/gannet/style.cljs
@@ -0,0 +1,17 @@
+(ns gannet.style)
+
+(def form-input
+  {:padding "5px"
+   :outline "transparent"
+   :border "2px solid #DADADA"
+   :width "280px"})
+
+(def form-button
+  {:padding "7px"
+   :background "#6F6D6D"
+   :border "1px solid #DADADA"
+   :color "white"
+   :width "50px"
+   :cursor "pointer"})
+
+
