cf6ef741583a0f6c348e42a5ed2276aeaca53afd
yang-wei <yangweilim21@gmail.com>
2016-05-04 19:01:30 +0900

    Render the data using fixed-data-table !

--- front/gannet/components/result_table.cljs
+++ front/gannet/components/result_table.cljs
@@ -1,9 +1,40 @@
 (ns gannet.components.result-table
   (:require [reagent.core :as r]
             [gannet.style :as style]
-            [ajax.core :refer [POST]]))
+            [cljsjs.fixed-data-table]))
 
-(defn component [links]
-  [:ul
-   (for [link links]
-     [:li (:url link) " " (:status link)])])
+(enable-console-print!)
+
+(def Table(r/adapt-react-class js/FixedDataTable.Table))
+(def Column (r/adapt-react-class js/FixedDataTable.Column))
+(def Cell (r/adapt-react-class js/FixedDataTable.Cell))
+
+(defn get-value [data index key] (-> data (nth index) (get key) str))
+
+(defn header-cell [{:keys [title]}]
+  (fn [props]
+    (r/as-element [Cell title])))
+
+(defn text-cell [{:keys [row-index field data]}]
+  (fn [props]
+    (let [index (.-rowIndex props)
+          keyword-field (keyword field)
+          value (get-value data index keyword-field)]
+      (r/as-element [Cell value]))))
+
+(defn component [data]
+  (let [url #(:url %)
+        status #(:status %)
+        rows-count (count data)] 
+    (when (> rows-count 0)
+      [Table {:width 500
+              :height 400
+              :rowHeight 50
+              :headerHeight 50
+              :rowsCount rows-count}
+       [Column {:cell (text-cell {:data data :field "url"})
+                :header (header-cell {:title "URL"})
+                :width 400}]
+       [Column {:cell (text-cell {:data data :field "status"})
+                :header (header-cell {:title "Status"})
+                :width 100}]])))
